7cf9b059eb016f0050d2b0e945a50a242ceab0f4
phm6530 <squirrel309@naver.com>
2024-10-17 11:49:21 +0900

    cors front 허용

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // cors
+  app.enableCors({
+    origin: process.env.CORS_ALLOWED_ORIGINS, // 허용할 도메인
+    credentials: true, // 쿠키 허용 여부 (선택)
+    methods: 'GET,POST,PUT,DELETE', // 허용할 메서드 지정
+    allowedHeaders: 'Content-Type, Authorization', // 허용할 헤더
+  });
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
